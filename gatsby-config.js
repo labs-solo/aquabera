1fda0e0ce63a1664e999f91649bd319f28ef7dd2
Samuel <Samuel.Mendenhall@gmail.com>
2022-10-25 14:26:58 -0400

    Updating text per request from Bryan (#40)
    Co-authored-by: Samuel Mendenhall <samuel@samuels-mbp.lan>

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Grow any token with greedy liquidity`,
-    description: `Grow any token with greedy liquidity`,
+    title: `Grow any token with low-slippage, on-chain liquidity`,
+    description: `Grow any token with low-slippage, on-chain liquidity`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -9,23 +9,23 @@ module.exports = {
       resolve: "gatsby-plugin-google-tagmanager",
       options: {
         id: "GTM-P4CNB46",
-  
+
         // Include GTM in development.
         //
         // Defaults to false meaning GTM will only be loaded in production.
         includeInDevelopment: false,
-  
+
         // datalayer to be set before GTM is loaded
         // should be an object or a function that is executed in the browser
         //
         // Defaults to null
         defaultDataLayer: { platform: "gatsby" },
-  
+
         // Specify optional GTM environment details.
         // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
         // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
         // dataLayerName: "YOUR_DATA_LAYER_NAME",
-  
+
         // Name of the event that is triggered
         // on every Gatsby route change.
         //
@@ -36,7 +36,7 @@ module.exports = {
         // Defaults to https://www.googletagmanager.com
         // selfHostedOrigin: "YOUR_SELF_HOSTED_ORIGIN",
       },
-    },    {
+    }, {
       resolve: `gatsby-plugin-styled-components`,
       options: {
         minify: false, // Breaks styles if not set to false
