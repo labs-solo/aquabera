58da38439673777994e14597628f50b2b54d17ba
tstemko <57684389+tstemko@users.noreply.github.com>
2023-06-12 22:16:54 -0400

    Rotating vaults banner (#48)
    * Change list of major vaults for banner
    
    * Rotating banner
    
    ---------
    
    Co-authored-by: Tatyana Stemkovski <tanya_stemkovski@hotmail.com>
    Co-authored-by: vastreck <78502722+vastreck@users.noreply.github.com>

--- src/containers/Banner.tsx
+++ src/containers/Banner.tsx
@@ -7,6 +7,8 @@ import { REFRESH_INTERVALS } from 'common/constants/constants';
 import { QueryItemsResponse, QueryName } from 'common/models/graphql';
 import { MonitorVaults } from 'common/models/farm';
 import { listMonitorVaultsQuery } from 'graphql/queries/listMonitorVaults.query';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 type Props = {
 }
@@ -26,46 +28,48 @@ const Banner: React.FC<Props> = (props) => {
   // only display major vaults: USDC, wBTC, wETH, wStETH, wMATIC, Chainlink
   const isMajorVault = (address: string) => {
     return (
-    //   address === '0x4aEF5144131dB95c110af41c8Ec09f46295a7C4B'
-    // || address === '0xFc089714519E84B7ce0a4023bfEE0D99F6d767dA'
-    // || address === '0x711901e4b9136119Fb047ABe8c43D49339f161c3'
-    // || address === '0x3ac9b3db3350A515c702ba19a001d099d4a5F132'
-    // || address === '0xDD2521755a8eb05c41C819488D1890e32FeB8753'
-    // || address === '0x9C90975B13d04D7d535359887C236ac51f2298cE'
-    address === '0x9ff3C1390300918B40714fD464A39699dDd9Fe00'
+      address === '0x4aEF5144131dB95c110af41c8Ec09f46295a7C4B'
+    || address === '0xFc089714519E84B7ce0a4023bfEE0D99F6d767dA'
+    || address === '0x711901e4b9136119Fb047ABe8c43D49339f161c3'
+    || address === '0x3ac9b3db3350A515c702ba19a001d099d4a5F132'
+    || address === '0xDD2521755a8eb05c41C819488D1890e32FeB8753'
+    || address === '0x9C90975B13d04D7d535359887C236ac51f2298cE'
+    || address === '0x9ff3C1390300918B40714fD464A39699dDd9Fe00'
     || address === '0x692437de2cAe5addd26CCF6650CaD722d914d974'
     );
   };
 
-  const vault = vaults.filter(v => isMajorVault(v.address))
-    .reduce( ( (prev, current) => ((prev.vaultIrrAllTx) > (current.vaultIrrAllTx)) ? prev : current), 
-    { address: '',
-      displayName: '',
-      vaultIrrAllTx: 0,
-    }); 
+  const vaultsToShow = vaults.filter(v => (isMajorVault(v.address) && v.vaultIrrAllTx > 0 ));
 
-  const maxAPY = parseInt(vault.vaultIrrAllTx.toString());
-  const displayName = vault.displayName.replace('(polygon)', '');
-  const bannerMessage = maxAPY 
-    ? `The ${displayName} vault has earned ${maxAPY}% IRR since inception. Click here to deposit`
-    : "Click here to deposit into a Vault"; 
-
-  const bannerLink = maxAPY
-    ? ( `https://app.ichi.org/vault?address=${vault.address}` )
-    : "https://app.ichi.org"
-
-  return (
+  return vaultsToShow.length > 0 ? (
     <div className="w-full m-auto overflow-hidden" id="home">
       <div 
         className="flex flex-row justify-center items-center pt-1 pb-1 pl-4 pr-4" 
         style={{backgroundImage: 'linear-gradient(to left, #197CFB, #1DAFD9)'}} >
-
-        <OutboundLink href={bannerLink} target="_blank" className="color-white text-center font-semibold">
-          {bannerMessage} &rarr;
+        <Carousel autoPlay={true} infiniteLoop={true} showArrows={false} showIndicators={false} showStatus={false} showThumbs={false} 
+          interval={6000} transitionTime={800}>
+          {vaultsToShow.map((v) => (
+            <div className="w-full" key={v.address}>
+              <OutboundLink href={`https://app.ichi.org/vault?address=${v.address}`} 
+                target="_blank" className="color-white text-center font-semibold center">
+                {`The ${v.displayName.replace('(polygon)', '')} vault has earned ${parseInt(v.vaultIrrAllTx.toString())}% IRR`} <br className='md:hidden'/> since inception. Click here to deposit &rarr;
+              </OutboundLink>
+            </div>
+          ))}
+        </Carousel>        
+      </div>
+    </div>        
+  ) : (
+    <div className="w-full m-auto overflow-hidden" id="home">
+      <div 
+        className="flex flex-row justify-center items-center pt-1 pb-1 pl-4 pr-4" 
+        style={{backgroundImage: 'linear-gradient(to left, #197CFB, #1DAFD9)'}} >
+        <OutboundLink href={`https://app.ichi.org`} 
+          target="_blank" className="color-white text-center font-semibold">
+          {`Click here to deposit into a Vault`} &rarr;
         </OutboundLink>
       </div>
-   </div>        
-
+    </div>        
   );
 };
 
